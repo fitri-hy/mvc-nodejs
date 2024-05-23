5bf48d38839682cbf6a87de44f859c68ce618f63
fitri-hy <noreply.orzpartners@gmail.com>
2024-05-23 09:14:46 +0700

    update

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const toDoListRoutes = require('./routes/toDoListRoutes');
 const pageRoutes = require('./routes/pageRoutes');
 
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
