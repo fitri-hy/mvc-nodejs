5305eeb50de1e2f252e9c6f63ecce567c70ae8ec
fitri-hy <noreply.orzpartners@gmail.com>
2024-05-24 17:24:13 +0700

    update

--- app.js
+++ app.js
@@ -2,14 +2,47 @@ const express = require('express');
 const path = require('path');
 const compression = require('compression');
 const cacheHeaders = require('express-cache-headers');
+const sharp = require('sharp');
+const fs = require('fs');
 const app = express();
 const toDoListRoutes = require('./routes/toDoListRoutes');
 const pageRoutes = require('./routes/pageRoutes');
+
+const convertToWebP = (req, res, next) => {
+    const publicDir = path.join(__dirname, 'public');
+    const filePath = path.join(publicDir, req.url);
+    const ext = path.extname(filePath);
+    const imageDirectories = ['images/logo', 'images/uploads'];
+    const isImage = imageDirectories.some((dir) => req.url.startsWith(`/${dir}`));
+
+    if (isImage && (ext === '.jpg' || ext === '.jpeg' || ext === '.png')) {
+        const webpFilePath = filePath.replace(/\.(jpg|jpeg|png)$/, '.webp');
+
+        fs.access(webpFilePath, fs.constants.F_OK, (err) => {
+            if (!err) {
+                res.type('image/webp').sendFile(webpFilePath);
+            } else {
+                sharp(filePath)
+                    .webp({ quality: 80 })
+                    .toFile(webpFilePath, (err) => {
+                        if (err) {
+                            console.error('Error converting image to WebP:', err);
+                            return next();
+                        }
+                        res.type('image/webp').sendFile(webpFilePath);
+                    });
+            }
+        });
+    } else {
+        next();
+    }
+};
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(convertToWebP);
 app.use(compression({ level: 9 }));
 app.use(cacheHeaders({
     cacheControl: {
